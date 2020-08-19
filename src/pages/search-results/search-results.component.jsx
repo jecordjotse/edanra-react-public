30cb173922e9a766fb9283e9914e2f5a9b0739a5
Nodenkey <nodenkey@gmail.com>
2020-08-19 17:18:11 +0000

    added s earch again

--- src/pages/search-results/search-results.component.jsx
+++ src/pages/search-results/search-results.component.jsx
@@ -12,6 +12,7 @@ import Navbar from "../../components/navbar/navbar.component";
 import Footer from "../../components/footer/footer.component";
 import {Helmet} from "react-helmet";
 import ScrollToTop from "../../utils/scroll-to-top";
+import FormInputSlider from "../../components/form-slider/form-slider.component";
 
 const SearchResultsPage = ({regions, districts, getResults, results, allProperties}) => {
 
@@ -21,7 +22,11 @@ const SearchResultsPage = ({regions, districts, getResults, results, allProperti
     const [filters, setFilters] = useState({
         town: "",
         region: "",
-        district: ""
+        district: "",
+        no_of_bedrooms: "",
+        property_type: "",
+        price_min: 20,
+        price_max: 10000,
     });
 
     const handleSubmit = event => {
@@ -29,10 +34,21 @@ const SearchResultsPage = ({regions, districts, getResults, results, allProperti
         getResults({filters, allProperties});
     };
 
+    const sliderChange = (event) => {
+        //event.preventDefault();
+        setFilters({
+            ...filters,
+            price_min: event[0],
+            price_max: event[1],
+        });
+    };
+
     const handleChange = event => {
         const {name} = event.target;
         let {value} = event.target;
-        value = value === "CHOOSE REGION" ? "" : value;
+        if (value === "CHOOSE REGION" || value === "PROPERTY TYPE"){
+            value = ""
+        }
         setFilters({
             ...filters,
             [name]: value
@@ -61,7 +77,7 @@ const SearchResultsPage = ({regions, districts, getResults, results, allProperti
                         <div className="col-12 col-md-4 form-group">
                             <input onChange={handleChange} className="custom-search-inputs" id="town" type="text"
                                    name="town"
-                                   placeholder="Search Town: Eg, Accra"
+                                   placeholder="SEARCH TOWN: Eg, Accra"
                                    style={{
                                        width: '100%',
                                        paddingBottom: '0',
@@ -105,10 +121,50 @@ const SearchResultsPage = ({regions, districts, getResults, results, allProperti
                             </datalist>
                         </div>
                     </div>
+                    <div className="form-row">
+                        <div className="col-12 col-md-4 form-group">
+                            <input onChange={handleChange} className="custom-search-inputs" id="town" type="number"
+                                   name="town"
+                                   placeholder="NUMBER OF BEDROOMS"
+                                   style={{
+                                       width: '100%',
+                                       paddingBottom: '0',
+                                       marginTop: '5px',
+                                       height: '60px',
+                                       paddingLeft: '10px'
+                                   }}/>
+                        </div>
+
+                        <div className="col-12 col-md-4 form-group">
+                            <select onChange={handleChange} id="property_type"
+                                    className="custom-select rounded-0 input-md"
+                                    style={{width: '100%', paddingBottom: '0px', marginTop: '5px', height: '60px'}}
+                                    name="property_type">
+                                <option>PROPERTY TYPE</option>
+                                <option value='house'>House</option>
+                                <option value='hotel'>Hotel</option>
+                                <option value='guest'>Guest House</option>
+                                <option value='hostel'>Hostel</option>
+                                <option value='apartment'>Apartment</option>
+                            </select>
+                        </div>
+
+                        <div className="col-12 col-md-4 form-group">
+                            <FormInputSlider
+                                price={[filters.price_min, filters.price_max]}
+                                label="Price (GHS)"
+                                id="price_slider"
+                                max={10000}
+                                min={20}
+                                sliderChange={sliderChange}
+                            />
+                        </div>
+                    </div>
 
                     <div className="row">
                         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}
                              className="form-group col-12">
+                            {/*<CustomButton inverted style={{marginRight: '50px'}}>More Options</CustomButton>*/}
                             <CustomButton type='submit'>Search</CustomButton>
                         </div>
                     </div>
