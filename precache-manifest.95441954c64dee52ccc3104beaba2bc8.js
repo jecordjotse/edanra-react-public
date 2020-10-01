9daf251e64a822d63e98a4f808b249a43c4ec1f2
jecordjotse <jeromecordjotse@gmail.com>
2020-10-01 10:15:14 +0000

    Updates

---
+++ precache-manifest.95441954c64dee52ccc3104beaba2bc8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c00466ec25b0a9d17d133aea458812",
+    "url": "/edanra-react-public/index.html"
+  },
+  {
+    "revision": "3fef5ec3f2ef9439164a",
+    "url": "/edanra-react-public/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "394761e34cc25b171025",
+    "url": "/edanra-react-public/static/css/main.37c59144.chunk.css"
+  },
+  {
+    "revision": "3fef5ec3f2ef9439164a",
+    "url": "/edanra-react-public/static/js/2.265aafb7.chunk.js"
+  },
+  {
+    "revision": "bd46c72820684c2c10a038d315dcd434",
+    "url": "/edanra-react-public/static/js/2.265aafb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394761e34cc25b171025",
+    "url": "/edanra-react-public/static/js/main.ef58b18e.chunk.js"
+  },
+  {
+    "revision": "221d4c9cb39f2357b79a",
+    "url": "/edanra-react-public/static/js/runtime-main.1712cb76.js"
+  },
+  {
+    "revision": "2539b1262ca8fc2fd2351371f745e2b3",
+    "url": "/edanra-react-public/static/media/african-american-woman-black-girl-black-woman-chair-1181414.2539b126.jpg"
+  },
+  {
+    "revision": "33a9e5a4eb2f8c554dd356e9e10b3801",
+    "url": "/edanra-react-public/static/media/chair.33a9e5a4.jpg"
+  },
+  {
+    "revision": "e2e62dc5fcef6b5fc1dca90b28d974d0",
+    "url": "/edanra-react-public/static/media/edanra.e2e62dc5.png"
+  },
+  {
+    "revision": "bd07db93f3f204e089767ed5db7bf4fa",
+    "url": "/edanra-react-public/static/media/namnso-ukpanah-6UqJTfoXIq8-unsplash.bd07db93.jpg"
+  },
+  {
+    "revision": "03984e70c3f2fb507865f94715e8fae5",
+    "url": "/edanra-react-public/static/media/phone.03984e70.jpg"
+  },
+  {
+    "revision": "66ffd1611db82664829d9b22c60da9e2",
+    "url": "/edanra-react-public/static/media/red-morley-hewitt-rl44UgsiObY-unsplash.66ffd161.jpg"
+  },
+  {
+    "revision": "bf9fcc33263d6729591be9141c2a03b2",
+    "url": "/edanra-react-public/static/media/smartphone.bf9fcc33.png"
+  },
+  {
+    "revision": "38f5457fd2b853762d6da2bfdecb5d51",
+    "url": "/edanra-react-public/static/media/two-person-in-long-sleeved-shirt-shakehand-955395.38f5457f.jpg"
+  }
+]);
